6ba0aecfaa946b4be309f69887f829c301b8d3ce
Leonid Liadveikin <fantom2259@gmail.com>
2023-05-03 02:50:06 +0200

    Initial Commit

---
+++ SwiftUIMultiplatformDemo/SwiftUIMultiplatformDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMultiplatformDemoApp.swift
+//  SwiftUIMultiplatformDemo
+//
+//  Created by Leonid Lyadveykin on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMultiplatformDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
